cb33f4e7e1d3911a86323a5c051e0928b5a2d238
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-11-14 12:34:42 +0000

    Basic navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Dashboard from './Components/Dashboard';
+import Login from './Components/Login';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: 'dashboard',
+          element: <Dashboard />,
+        },
+        {
+          path: 'login',
+          element: <Login />,  
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <h1>Money Tracker</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
